5172e660f1390a1a2961741eb822a56b4f89c27d
trung1609 <vmt162005@gmail.com>
2026-04-13 20:17:05 +0700

    Initialize Customer-Service and Product-Service microservices with Gradle build system, separate configurations, and database connections.

---
+++ Session01/base-project/Customer-Service/src/test/java/com/api/trung/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.trung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Session01/base-project/Product-Service/src/main/java/com/api/trung/entity/Product.java
@@ -0,0 +1,32 @@
+package com.api.trung.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    private String url;
+
+    private String description;
+
+    private Integer quantity;
+
+    private Integer stock;
+}
