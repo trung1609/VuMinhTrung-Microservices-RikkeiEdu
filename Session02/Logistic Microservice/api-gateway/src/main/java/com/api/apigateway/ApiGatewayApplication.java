4c870b9853aae04947070a83af774ff2035f0fa4
trung1609 <vmt162005@gmail.com>
2026-04-29 19:38:45 +0700

    pull

--- Session02/Logistic Microservice/api-gateway/src/main/java/com/api/apigateway/ApiGatewayApplication.java
+++ Session02/Logistic Microservice/api-gateway/src/main/java/com/api/apigateway/ApiGatewayApplication.java
@@ -1,14 +1,15 @@
-package com.trung.orderservice;
+package com.api.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class OrderServiceApplication {
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OrderServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
 
 }
