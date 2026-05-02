31e503fb7b1285f87b352dbd0960859152d687c6
trung1609 <vmt162005@gmail.com>
2026-05-02 08:40:21 +0700

    Initialize medical-discovery-server with Eureka server setup, Gradle configuration, basic Spring Boot application structure, and essential IDE support files

---
+++ Session06/medical-discovery-server/src/test/java/com/api/medicaldiscoveryserver/MedicalDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.medicaldiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
